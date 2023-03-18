775514a5f2147cac44f772dc0986467bca6a9f38
mariusgeorgescu <georgescumarius@live.com>
2023-03-18 22:37:24 +0200

    updates

--- src/PokerGameApp.hs
+++ src/PokerGameApp.hs
@@ -15,13 +15,17 @@ import           Control.Monad.Reader      (MonadIO (..),
 import           Control.Monad.State       (MonadState, StateT (runStateT),
                                             evalStateT, get, put)
 
+import           Control.Monad             (replicateM_)
 import           Control.Monad.Error.Class (MonadError (throwError))
-import           PokerGame                 (PokerPlayerAction(..), PokerGame (_gamePlayerTurnIndex), addPlayerToGame, 
-                                            dealHands, initPokerGame, setDealer,
-                                            startPokerGame, postBlinds, inGameBettingAction)
+import           Data.Either.Extra         (maybeToEither)
+import           PokerGame                 (PokerGame (_gamePlayerTurnIndex),
+                                            PokerPlayerAction (..),
+                                            addPlayerToGame, dealHands,
+                                            inGameBettingAction, initPokerGame,
+                                            postBlinds, setDealer,
+                                            startPokerGame)
 import           System.Random             (Random (randomR), RandomGen,
-                                            newStdGen)
-import Data.Either.Extra (maybeToEither)
+                                            mkStdGen)
 
 newtype Env =
   Env
@@ -84,40 +88,13 @@ randomList gen n = go gen [0 .. n - 1]
       let (r, g') = randomR (0, n - x - 1) g
        in r : go g' xs
 
-initPlayer :: IO (String, Int)
-initPlayer = do
-  print "Enter your name: "
-  name <- getLine
-  print "Enter your chips: "
-  chips <- getLine
-  return (name, read chips)
-
-
--- testRun2 :: [Int] -> Either String PokerGame
--- testRun2 perms =
---   initPokerGame perms 10 >>= addPlayerToGame "Marius" 100 >>=
---   addPlayerToGame "Andrei" 100 >>=
---   startPokerGame >>=
---   setDealer 1 >>=
---   dealHands
 
 -- | Function to initate a PokerGame in NotStarted state, with a valid Deck and mininum bet value.
 testPokerApp :: PokerApp ()
 testPokerApp = do
-  minBet <- asks envMinBet
-  gen <- newStdGen
-  let permutations = randomList gen 51
-  liftIO $ print "Initiating Poker Game"
-  g <- liftEither $ initPokerGame permutations minBet
-  liftIO $ print g
-  liftIO $ print "Adding 1st player"
-  (p1name, p1chips) <- liftIO initPlayer
-  g1 <- liftEither $ addPlayerToGame p1name p1chips g
-  (p2name, p2chips) <- liftIO initPlayer
-  g2 <- liftEither $ addPlayerToGame p2name p2chips g1
-  (p3name, p3chips) <- liftIO initPlayer
-  g2 <- liftEither $ addPlayerToGame p3name p3chips g2
-  liftIO $ print g2
+  initGame
+  addPlayers 4
+  g2 <- get
   g3 <- liftEither $ startPokerGame g2
   liftIO $ print g3
   g4 <- liftEither $ setDealer 0 g3
@@ -136,3 +113,23 @@ testPokerApp = do
   g9 <- liftEither $ inGameBettingAction pt (Call 9) g8
   liftIO $ print g9
   return ()
+
+initGame :: PokerApp ()
+initGame = do
+  minBet <- asks envMinBet
+  let gen = mkStdGen 10
+  let permutations = randomList gen 51
+  liftIO $ print "INITIALISING POKER GAME"
+  g <- liftEither $ initPokerGame permutations minBet
+  put g
+
+addPlayer :: PokerApp ()
+addPlayer = do
+  name <- liftIO $ print "Enter your name: " >> getLine
+  liftIO $ print "100 chips by default"
+  g <- get
+  g1 <- liftEither $ addPlayerToGame name 100 g
+  put g1
+
+addPlayers :: Int -> PokerApp ()
+addPlayers n = replicateM_ n addPlayer
